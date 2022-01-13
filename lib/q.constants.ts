ac7f0f2355a139b19db94615d4d101b54cebfe65
andrii33 <andynew3@gmail.com>
2022-01-12 17:10:53 -0800

    add constants file declaration

---
+++ lib/q.constants.ts
@@ -0,0 +1,3 @@
+export const Q_PROCESS = Symbol.for('Q_PROCESS')
+export const Q_CONSUMER_METHOD = Symbol.for('Q_CONSUMER_METHOD')
+export const Q_CONSUMER_EVENT_HANDLER = Symbol.for('Q_CONSUMER_EVENT_HANDLER')
